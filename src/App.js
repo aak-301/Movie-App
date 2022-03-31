ef1a684341fbc8f0126969cea80faba1aa04ee25
aak-301 <76422079+aak-301@users.noreply.github.com>
2022-03-31 09:39:07 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -95,11 +95,11 @@ function App() {
     <Container>
       <Header>
         <AppName>
-          <MovieImage src="/react-movie-app/movie-icon.svg" />
+          <MovieImage src="/movie-icon.svg" />
           React Movie App
         </AppName>
         <SearchBox>
-          <SearchIcon src="/react-movie-app/search-icon.svg" />
+          <SearchIcon src="/search-icon.svg" />
           <SearchInput
             placeholder="Search Movie"
             value={searchQuery}
